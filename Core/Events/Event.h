94a13c7eeec1f7b9f42b40255a4ff35a0de6cc3b
timmypidashev <pidashev.tim@gmail.com>
2022-02-21 16:27:19 -0800

    Fix concatenation error after switching to g++ for compilations on unix systems

--- Core/Events/Event.h
+++ Core/Events/Event.h
@@ -31,7 +31,7 @@ namespace Iridescent {
 		EventCategoryMouseButton    = BIT(4)
 	};
 
-#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::type; }\
 								virtual EventType GetEventType() const override { return GetStaticType(); }\
 								virtual const char* GetName() const override { return #type; }
 
