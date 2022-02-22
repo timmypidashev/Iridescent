942a0ec59259c1ace5c1a15249376819c857d3e5
timmypidashev <pidashev.tim@gmail.com>
2022-02-21 18:44:35 -0800

    Add Layer sources

---
+++ Core/Layer/Layer.cpp
@@ -0,0 +1,16 @@
+#include "Iridpch.h"
+#include "Layer.h"
+
+namespace Iridescent {
+
+	Layer::Layer(const std::string& debugName)
+		: m_DebugName(debugName)
+	{
+	}
+
+	Layer::~Layer()
+	{
+
+	}
+
+} 
---
+++ Core/Layer/Layer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Exports.h"
+#include "Events/Event.h"
+
+namespace Iridescent {
+
+	class IRID_EXPORT Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer();
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate() {}
+		virtual void OnEvent(Event& event) {}
+
+		inline const std::string& GetName() const { return m_DebugName; }
+	protected:
+		std::string m_DebugName;
+	};
+
+} 
