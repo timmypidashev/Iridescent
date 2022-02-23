571803b4cf77078f6e95828bffaeb98c736ecd93
timmypidashev <pidashev.tim@gmail.com>
2022-02-22 20:12:18 -0800

    Fix adrress out of bounds error due to 'glclear'

--- Core/Application/Application.cpp
+++ Core/Application/Application.cpp
@@ -52,9 +52,6 @@ namespace Iridescent {
 	{
 		while (m_Running)
 		{
-            glClearColor(1, 0, 1, 1);
-			glClear(GL_COLOR_BUFFER_BIT);
-
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
 
