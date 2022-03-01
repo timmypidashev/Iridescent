007076552d05dbec9371aeafd9c209fe9a59554a
timmypidashev <pidashev.tim@gmail.com>
2022-03-01 15:04:43 -0800

    Initial Renderer implementation

---
+++ Core/Platform/OpenGL/OpenGLContext.cpp
@@ -0,0 +1,28 @@
+#include "Iridpch.h"
+#include "OpenGLContext.h"
+
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <GL/GL.h>
+
+namespace Iridescent {
+
+	OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
+		: m_WindowHandle(windowHandle)
+	{
+		IRID_CORE_ASSERT(windowHandle, "Window handle is null!")
+	}
+
+	void OpenGLContext::Init()
+	{
+		glfwMakeContextCurrent(m_WindowHandle);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		IRID_CORE_ASSERT(status, "Failed to initialize Glad!");
+	}
+
+	void OpenGLContext::SwapBuffers()
+	{
+		glfwSwapBuffers(m_WindowHandle);
+	}
+
+} 
--- Core/Window/Windows.cpp
+++ Core/Window/Windows.cpp
@@ -6,6 +6,8 @@
 #include "Events/MouseEvent.h"
 #include "Events/KeyEvent.h"
 
+#include "Platform/OpenGL/OpenGLContext.h"
+
 namespace Iridescent {
 	
 	static bool s_GLFWInitialized = false;
@@ -48,7 +50,10 @@ namespace Iridescent {
 		}
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-		glfwMakeContextCurrent(m_Window);
+
+		m_Context = new OpenGLContext(m_Window);
+		m_Context->Init();
+
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
@@ -97,7 +102,7 @@ namespace Iridescent {
 			}
 		});
 
-        glfwSetCharCallback(m_Window, [](GLFWwindow* window, unsigned int keycode)
+		glfwSetCharCallback(m_Window, [](GLFWwindow* window, unsigned int keycode)
 		{
 			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
 
@@ -151,7 +156,7 @@ namespace Iridescent {
 	void Windows::OnUpdate()
 	{
 		glfwPollEvents();
-		glfwSwapBuffers(m_Window);
+		m_Context->SwapBuffers();
 	}
 
 	void Windows::SetVSync(bool enabled)
